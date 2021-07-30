f75b028e2aa70717b2dfc1ac29eb582ae5224ac3
toddiuzho <todd.trimmer@gmail.com>
2021-07-30 12:15:32 -0500

    set log levels

--- package.sh
+++ package.sh
@@ -2,8 +2,6 @@
 # Terminal Logging.
 #
 
-#=import color
-
 declare -i \
   LOG_LEVEL_FATAL=0 \
   LOG_LEVEL_ERROR=10 \
@@ -22,12 +20,20 @@ log-info() {
   log-is-info-enabled && echo -e "[${COL_MAGENTA}INFO${COL_CLEAR}] $*"
 }
 
+log-set-info() {
+  LOG_LEVEL=$LOG_LEVEL_INFO
+}
+
 log-is-debug-enabled() {
   [ $LOG_LEVEL -ge $LOG_LEVEL_DEBUG ]
 }
 
 log-debug() {
   log-is-debug-enabled && echo -e "[${COL_BLUE}INFO${COL_CLEAR}] $*"
+}
+
+log-set-debug() {
+  LOG_LEVEL=$LOG_LEVEL_DEBUG
 }
 
 log-is-warn-enabled() {
@@ -38,6 +44,10 @@ log-warn() {
   log-is-warn-enabled && echo -e "[${COL_YELLOW}WARN${COL_CLEAR}] $*" >&2
 }
 
+log-set-warn() {
+  LOG_LEVEL=$LOG_LEVEL_WARN
+}
+
 log-is-error-enabled() {
   [ $LOG_LEVEL -ge $LOG_LEVEL_ERROR ]
 }
@@ -46,8 +56,16 @@ log-error() {
   log-is-error-enabled && echo -e "[${COL_RED}ERROR${COL_CLEAR}] $*" >&2
 }
 
+log-set-error() {
+  LOG_LEVEL=$LOG_LEVEL_ERROR
+}
+
 log-fatal() {
   echo -e "${COL_RED}[FATAL] $* ${COL_CLEAR}" >&2
+}
+
+log-set-fatal() {
+  LOG_LEVEL=$LOG_LEVEL_FATAL
 }
 
 emphasize() {
